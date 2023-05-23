b00913a42ebd8f784524810e22b484f5fbc9b5d9
jobrane33 <83889965+jobrane33@users.noreply.github.com>
2023-05-23 19:57:11 +0100

    add gui folder

---
+++ src/tn/eduVision/GUI/GuiExample.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tn.eduVision.GUI;
+
+/**
+ *
+ * @author job_j
+ */
+public class GuiExample {
+    
+}
