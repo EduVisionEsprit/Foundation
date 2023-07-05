98f914a36b6c10cc37ff9dc1b0d983b3a339adca
SanaBenHammouda <133594690+SanaBenHammouda@users.noreply.github.com>
2023-07-06 00:27:45 +0100

    Create TEST_NoteEtudiant.java

--- src/tn/eduVision/GUI/TEST_NoteEtudiant.java
+++ src/tn/eduVision/GUI/TEST_NoteEtudiant.java
@@ -8,6 +8,7 @@ package tn.eduVision.GUI;
 
  
 import javafx.application.Application;
+import static javafx.application.Application.launch;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
@@ -23,15 +24,7 @@ import javafx.stage.Stage;
  * @author bhsan
  */
 public class TEST_NoteEtudiant extends Application {
-    /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
 
-/**
- *
- * @author bhsan
- */
     
     @Override
 public void start(Stage primaryStage) throws Exception {
