81bfdd524e520a452373549aafe8097043a5f68e
SanaBenHammouda <133594690+SanaBenHammouda@users.noreply.github.com>
2023-07-06 02:03:23 +0100

    updates

---
+++ src/tn/eduVision/GUI/Main.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tn.eduVision.GUI;
+
+/**
+ *
+ * @author job_j
+ */
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import tn.eduVision.GUI.*;
+
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("calander.fxml"));
+        Parent root = loader.load();
+
+        primaryStage.setTitle("Calendar");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
+
---
+++ src/tn/eduVision/test/DbConnectionTest.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tn.eduVision.test;
+
+import java.sql.Connection;
+import java.util.List;
+import tn.eduVision.entités.Materiel;
+import tn.eduVision.entités.Salle;
+import tn.eduVision.entités.TypeRessource;
+import tn.eduVision.entités.TypeSalle;
+import tn.eduVision.tools.SqlConnectionManager;
+import tn.eduVision.entités.TypeRessource;
+import tn.eduVision.services.MaterielService;
+
+/**
+ *
+ * @author job_j
+ */
+public class DbConnectionTest {
+    public static void main(String args[]){
+        //SqlConnectionManager connectionManager = SqlConnectionManager.getInstance();
+        //Connection connection = connectionManager.getConnection();
+        //sould only log info once
+        //Connection connection2 = connectionManager.getConnection();
+        MaterielService ms = new MaterielService();
+        Materiel m = new Materiel("test", 10, TypeRessource.Salle, 99);
+        //ms.add(m);
+        ms.update(m);
+        Salle s = new Salle("aaa", 75, "test", "test", TypeSalle.Amphi, 7);
+        SallesService ss = new SallesService();
+        
+        //ss.delete(s);
+        
+        
+        System.out.println(ss.getAll());
+        
+        
+        
+    }
+}
